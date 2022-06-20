2d89b9b0a3580aeac76aecc1e27ff5729d1d04f2
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-06-20 16:54:51 -0300

    troca de gerenciador npm para yarn e adição de pacotes para a navegação stack

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/Home';
+import ISSTrackerScreen from './screens/IssLocation';
+import MeteorScreen from './screens/Meteor';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MeteorScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
