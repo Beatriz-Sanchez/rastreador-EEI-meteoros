2feff7860c025d54b9776915be435ead68d3d0a4
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-06-20 17:41:59 -0300

    navegação stack adicionada

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createStackNavigator } from '@react-navigation/stack'; 
 import HomeScreen from './screens/Home';
 import ISSTrackerScreen from './screens/IssLocation';
 import MeteorScreen from './screens/Meteor';
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <MeteorScreen/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="IssLocation" component={ISSTrackerScreen}/>
+        <Stack.Screen name="Meteors" component={MeteorScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
