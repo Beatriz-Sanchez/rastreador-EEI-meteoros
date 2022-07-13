517294f209815df8ef4483b3fbd3210be3100695
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-13 11:17:43 -0300

    imagem de fundo e título adicionados

--- screens/IssLocation.js
+++ screens/IssLocation.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, ImageBackground, StatusBar, SafeAreaView, Platform } from 'react-native';
+import MapView, { Marker } from 'react-native-maps'
 
 export default class ISSTrackerScreen extends Component {
     render(){
         return(
             <View style ={styles.container}>
-                <Text> Tela de Rasteador de ISS </Text>
+                <SafeAreaView styles={styles.droidSafeArea}/>
+                <ImageBackground source={require('../assets/iss_bg.jpg')} style={styles.backgroundImage}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.titleText}>Localização da EEI</Text>
+                    </View>
+                </ImageBackground>
             </View>
         );
     }
@@ -13,9 +19,43 @@ export default class ISSTrackerScreen extends Component {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#faf',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1
     },
-  });
+    droidSafeArea: {
+       marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+    },
+    titleContainer: {
+        flex: 0.1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    titleText: {
+        fontSize: 30,
+        fontWeight: "bold",
+        color: "white"
+    },
+    mapContainer: {
+        flex: 0.7
+    },
+    map: {
+        width: "100%",
+        height: "100%"
+    },
+    infoContainer: {
+        flex: 0.2,
+        backgroundColor: 'white',
+        marginTop: -10,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 30
+    },
+    infoText: {
+        fontSize: 15,
+        color: "black",
+        fontWeight: "bold"
+    }
+})
