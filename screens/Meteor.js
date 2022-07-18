fd98b26a4b65fd02e0caeb8fd5caf85854f42744
Beatriz Lisboa Sanchez <bia.lsanchez@gmail.com>
2022-07-18 16:01:07 -0300

    adicionada chamada de APIna tela Meteoros

--- screens/Meteor.js
+++ screens/Meteor.js
@@ -1,21 +1,52 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import axios from 'axios';
 
 export default class MeteorScreen extends Component {
-    render(){
-        return(
-            <View style ={styles.container}>
-                <Text> Tela de meteoros </Text>
-            </View>
-        );
+  constructor() {
+    super();
+    this.state = {
+      meteors: {},
+    };
+  }
+  getMeteores = async () => {
+    try {
+      let response = await axios.get(
+        "https://api.nasa.gov/neo/rest/v1/feed?api_key=FF5YDCmGg6ZHo2yOCkiHXqSGnq5WkiupCxuoQuTA"
+      );
+      this.setState({ meteors: response.data.near_earth_objects });
+    } catch (error) {
+      alert(error.message);
+      console.log(error.message);
     }
+  };
+
+  componentDidMount(){
+    this.getMeteores();
+  }
+
+  render() {
+    if (Object.keys(this.state.meteors).length === 0) {
+        return (
+            <View style={styles.container}>
+              <Text> Carregando... </Text>
+            </View>
+          );
+    } else {
+        return (
+            <View style={styles.container}>
+              <Text> Chamada realizada! </Text>
+            </View>
+          );
+    }
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#ffa',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#ffa",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
